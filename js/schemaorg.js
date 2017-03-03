4e036f9a0ce4c28f4c8569662d85b32027247196
tomerbarkan <tomerbarkan@gmail.com>
2017-03-03 08:45:27 +0200

    Schema org added

---
+++ js/schemaorg.js
@@ -0,0 +1,54 @@
+{
+	"@context": "http://schema.org",
+	"@type": "VideoGame",
+	"name": [
+		{
+			"@language": "en",
+			"@value": "Judgment: Apocalypse Survival Simulation"
+		}
+	],
+	"description": [
+		{
+			"@language": "en",
+			"@value": "Manage a society of survivors in a demon apocalypse. Survive by building a base, researching science and the occult, crafting equipment and exploring a world overrun by demons."
+		}
+	],
+	"applicationCategory": [
+		"Game"
+	],
+	"operatingSystem": [
+		"Windows"
+	],
+	"playMode": [
+		"SinglePlayer"
+	],
+	"genre": [
+		"Simulation",
+		"Management",
+		"2D",
+		"Real-time tactics (RTT)",
+		"Real-time strategy (RTS)",
+		"Construction and management simulation",
+		"Sandbox"
+	],
+	"gamePlatform": [
+		"https://en.wikipedia.org/wiki/Microsoft_Windows"
+	],
+	"url": "http://www.judgmentgame.com",
+	"image": "http://www.judgmentgame.com/img/poster.jpg",
+	"screenshot": "http://www.judgmentgame.com/img/background.jpg",
+	"sameAs": [
+		"https://en.wikipedia.org/wiki/Judgment:_Apocalypse_Survival_Simulation",
+		"http://store.steampowered.com/app/455980",
+		"http://judgment.wikia.com/wiki/Judgment:_Apocalypse_Survival_Simulation_Wikia"
+	],
+	"trailer": {
+		"@type": "VideoObject",
+		"url": "https://www.youtube.com/watch?v=iggP0Fewa-4",
+		"inLanguage": "en",
+		"name": "Judgment: Apocalypse Survival Simulation - alpha trailer Dec 2016",
+		"description": "Rebuild society. Survive the Apocalypse. Judgment: Apocalypse Survival Simulation is a colony simulation game with tactical combat that takes place in the midst of a demon apocalypse. The gates of hell have opened and unleashed relentless demons into the world -- but you can fight back! Guide a group of survivors through the mayhem -- hiding from hellspawn and building a sanctuary. Survive by gathering resources, crafting equipment, defending your base and sending teams to scavenge for supplies. Research human technologies and practice the dark arts to find a way and send the demons back to hell. To survive is to prevail. Will you survive Judgment day?",
+		"thumbnailUrl": "http://i3.ytimg.com/vi/iggP0Fewa-4/hqdefault.jpg",
+		"uploadDate": "2016-02-27"
+	}
+}
